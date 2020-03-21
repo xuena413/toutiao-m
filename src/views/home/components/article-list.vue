669b8358ab65913b6f8f3cc65b68b0d5edba1310
xuena413 <813638180@qq.com>
2020-03-21 22:31:58 +0800

    上拉加载、下拉刷新的 真实数据显示绑定

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -3,32 +3,39 @@
   <!-- scrollwrapper为了阅读记忆 -->
   <div class="scroll-wrapper">
     <!-- 下拉刷新组件包裹 列表组件 -->
-    <van-pull-refresh v-model="downLoading" @refresh="onRefresh" success-text="successText">
+    <van-pull-refresh v-model="downLoading" @refresh="onRefresh" :success-text="successText">
       <van-list finished-text="暂无数据" offset="200" v-model="upLoading" :finished="finished" @load="onLoad">
        <van-cell-group>
          <!------------------------------------ 假数据------------------------------>
         <!-- <van-cell v-for="item in articles" :key="item" title="美股又垄断了" :value="'天台排队'+item" > -->
-           <van-cell v-for="item in articles" :key="item.art_id">
+           <van-cell v-for="item in articles" :key="item.art_id.toString()">
           <!-- 无图/单图/三图 -->
           <div class="article_item">
             <!-- 标题 -->
-            <h3 class="van-ellipsis">2020年是世界大变革</h3>
+            <h3 class="van-ellipsis">{{item.title}}</h3>
+            <!-- 根据封面类型决定 3/1/0 -->
             <!-- 三图图片 -->
-            <div class="img_box">
+            <div class="img_box" v-if="item.cover.type===3">
               <!-- 图片组件用的是vant的图片组件  需要使用该组件进行图片的懒加载-->
+                <!-- 三图图片 -->
+                <!----------------------------------1.动态加载 -->
+              <!-- <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg"/>
               <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg"/>
-              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg"/>
-              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg"/>
+              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg"/> -->
+
+              <van-image class="w33" fit="cover" :src="item.cover.images[0]"/>
+              <van-image class="w33" fit="cover" :src="item.cover.images[1]"/>
+              <van-image class="w33" fit="cover" :src="item.cover.images[2]"/>
             </div>
-            <!-- 单图 -->
-             <!-- <div class="img_box">
-              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg"/>
-            </div> -->
+            <!-- 单图 --> -->
+             <div class="img_box" v-if="item.cover.type===1" >
+              <van-image class="w100" fit="cover"  :src="item.cover.images[0]"/>
+            </div>
             <!-- 作者信息 -->
             <div class="info_box">
-              <span>你像一阵风</span>
-              <span>8评论</span>
-              <span>10分钟前</span>
+              <span>{{item.aut_name}}</span>
+              <span>{{item.comm_count}}</span>
+              <span>{{item.pubdate}}</span>
               <span class="close"><van-icon name="cross"></van-icon></span>
             </div>
 </div>
@@ -41,6 +48,8 @@
 
 <script>
 import { getArticles } from '@/api/articles'
+// 引入请求模块 article
+
 export default {
   // prop:['channel_id'] 字符串数组接收，接收方式，比较简单
   props: {
@@ -97,7 +106,7 @@ export default {
         timestamp: this.timestamp || Date.now()
 
       }) // 指的当前的频道  =》（有历史时间戳用历史的没有就用当前的）
-      this.articles.push(data.results) // 将数据追加到队尾
+      this.articles.push(...data.results) // 将数据追加到队尾  push一项一项的内容 （数组里面）
       this.upLoading = false
       // 需要判断历史时间戳是否为0，0就是没数据  this.upLoading = false
       if (data.pre_timestamp) {
@@ -109,16 +118,38 @@ export default {
       }
     },
     // 下拉刷新
-    onRefresh () {
-      setTimeout(() => {
-        // 读取最新的数据 而且最新的数据要添加数据头部
-        const arr = Array.from(Array(2), (value, index) => '追加' + (index + 1))
-        // 数组添加到头部
-        this.articles.unshift(...arr)
-        // 手动的关闭正在加载的状态
-        this.downLoading = false
-        this.successText = `更新了${arr.length}条数据`
-      }, 1000)
+    // onRefresh () {
+    //   setTimeout(() => {
+    //     // 读取最新的数据 而且最新的数据要添加数据头部
+    //     const arr = Array.from(Array(2), (value, index) => '追加' + (index + 1))
+    //     // 数组添加到头部
+    //     this.articles.unshift(...arr)
+    //     // 手动的关闭正在加载的状态
+    //     this.downLoading = false
+    //     this.successText = `更新了${arr.length}条数据`
+    //   }, 1000)
+    // }
+    // ------------------------------------------下拉动态数据更新
+    async onRefresh () {
+      // 下拉发送最新的时间戳
+      const data = await getArticles({
+        channel_id: this.channel_id,
+        timestamp: Date.now()
+      })
+      this.downLoading = false
+      if (data.results.length) {
+        this.articles = data.results // 历史数据被全部覆盖
+        // 此时你 已经之前的全部覆盖了 假如你之前把数据拉到了底端 也就意味着 finished=true
+        //  this.finished= data.pre_timestamp?
+        if (data.pre_timestamp) {
+          this.finished = false // 重新唤醒列表，让列表可以继续上拉加载
+          this.timestamp = data.pre_timestamp
+        }
+        this.successText = `更新了${data.results.length}条数据`
+      } else {
+        this.successText = '当前已经是最新数据'
+      }
+      // 需要判断最新的时间戳能否换来数据  把新数据替换旧数据 如果不能 就告诉大家暂时没有更新
     }
   }
 }
